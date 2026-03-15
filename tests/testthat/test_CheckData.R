adfd47f7616ca129472ddead036a257f57f1c595
Stathis Gennatas <gennatas@gmail.com>
2026-03-14 23:02:19 -0700

    up

--- tests/testthat/test_CheckData.R
+++ tests/testthat/test_CheckData.R
@@ -1,9 +1,14 @@
 # test_CheckData.R
 # ::rtemis::
-# 2025 EDG rtemis.org
+# 2025- EDG rtemis.org
 
-# check_data() ----
-check_data(iris)
+# %% check_data() ----
 test_that("check_data() succeeds", {
-  expect_s7_class(check_data(iris), CheckData)
+  x <- data.frame(
+    a = c(1, 2, 3, NA),
+    b = c("A", "B", "C", "D"),
+    c = c(1.5, NA, 3.5, NA)
+  )
+  x_cd <- check_data(x, get_na_case_pct = TRUE, get_na_feature_pct = TRUE)
+  expect_s7_class(x_cd, CheckData)
 })
