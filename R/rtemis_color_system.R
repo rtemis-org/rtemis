6e64a81e84ac295357e9bd1f5ab01fb15818f566
Stathis Gennatas <gennatas@gmail.com>
2026-06-23 05:52:54 -0700

    add col_preprocessor, col_decom

--- R/rtemis_color_system.R
+++ R/rtemis_color_system.R
@@ -1,11 +1,12 @@
 # %% rtemis colors ----
-highlight_col <- rtemis_colors[["orange"]]
-col_object <- "#808080" # object name in repr_S7name
-col_info <- rtemis_colors[["blue"]]
-col_outer <- rtemis_colors[["purple"]]
-col_tuner <- rtemis_colors[["pink"]]
-col_calibrator <- rtemis_colors[["magenta"]]
-col_decom <- rtemis_colors[["purple"]]
+highlight_col <- rtemis_colors[["teal"]]
 col_success <- "#00cc8f" # session node ok
+col_info <- rtemis_colors[["blue"]]
 col_error <- rtemis_colors[["red"]] # session node error
 col_warn <- rtemis_colors[["orange"]] # session node aborted/warning
+
+col_preprocessor <- rtemis_colors[["juniper"]]
+col_decom <- rtemis_colors[["green"]]
+col_tuner <- rtemis_colors[["pink"]]
+col_outer <- rtemis_colors[["magenta"]]
+col_calibrator <- rtemis_colors[["purple"]]
